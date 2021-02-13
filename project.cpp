ae498d95188ed55d85a4ee6c1ed5c5f5a5bc02ad
daeun <58421346+qor4@users.noreply.github.com>
2021-02-13 21:11:33 +0900

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -585,7 +585,7 @@ int main(int argc, char** argv) {
 	glClearDepth(1.0f);
 	glutInitWindowSize(1000, 800);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("openGL");
+	glutCreateWindow("openGL Project");
 	init();
 
 	glutDisplayFunc(draw);
